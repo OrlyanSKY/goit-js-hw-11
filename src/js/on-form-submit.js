67cdb25fb50a571ecbf9159baa3fc758a2742c59
Denis Orlyansky <000slim001@gmail.com>
2022-08-28 23:03:55 +0300

    fix render

--- src/js/on-form-submit.js
+++ src/js/on-form-submit.js
@@ -32,22 +32,26 @@ async function pixabayAPI(nameRequest, page) {
   try {
     const data = await fetchPics(nameRequest, page);
     const hits = await data.hits;
+    console.log(hits.length);
     if (hits.length === 0) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
+      return;
     }
     renderMarcup(hits);
     observer.observe(guard);
     simpleGallery();
   } catch (error) {
-    Notify.warning('ERROR');
+    Notify.warning(
+      `We're sorry, but you've reached the end of search results.`
+    );
   }
 }
 
 function renderMarcup(data) {
   data.forEach(elem => {
-    galleryRef.innerHTML += gap(elem);
+    galleryRef.insertAdjacentHTML('beforeend', gap(elem));
   });
 }
 function updateGallery(entries) {
@@ -63,7 +67,9 @@ function simpleGallery() {
     captionDelay: 250,
     showCounter: false,
   });
-  gallery.on('show.simplelightbox');
+  gallery.on('show.simplelightbox', function () {
+    gallery.refresh();
+  });
 }
 // function logKey(evt) {
 //   if (evt.code === 'Enter') {
