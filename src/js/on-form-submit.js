fa74d392ca9112becd7d3801421b824986d913a8
Denis Orlyansky <000slim001@gmail.com>
2022-09-01 01:32:49 +0300

    fixes

--- src/js/on-form-submit.js
+++ src/js/on-form-submit.js
@@ -1,5 +1,5 @@
 import { Notify } from 'notiflix';
-
+import OnlyScroll from 'only-scrollbar';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -17,67 +17,19 @@ form.addEventListener('submit', onFormSbmt);
 let page = 1;
 let nameRequest = '';
 
+const scroll = new OnlyScroll(document.scrollingElement);
 const gallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
   showCounter: false,
 });
 
-const options = {
+const observOptions = {
   root: null,
   rootMargin: '100px',
   threshold: 0.5,
 };
-const observer = new IntersectionObserver(updateGallery, options);
-
-function onFormSbmt(evt) {
-  evt.preventDefault();
-
-  if (nameRequest === evt.currentTarget.elements.input.value) {
-    Notify.warning(`It's already found! May be another one?`);
-    return;
-  }
-
-  nameRequest = evt.currentTarget.elements.input.value;
-  evt.target.reset();
-  form.classList.remove('open');
-  galleryRef.innerHTML = '';
-  page = 1;
-
-  if (!nameRequest) {
-    Notify.warning('Please, enter a search term!');
-    evt.preventDefault();
-    return false;
-  }
-
-  pixabayAPI(nameRequest, page);
-}
-
-async function pixabayAPI(nameRequest, page) {
-  try {
-    const data = await fetchPics(nameRequest, page);
-    const hits = await data.hits;
-
-    renderMarkup(hits, galleryRef, gap);
-    gallery.refresh();
-    if (hits.length >= 40) {
-      observer.observe(guard);
-    }
-    if (page === 1 && hits.length > 1) {
-      Notify.info(`Hooray! We found ${data.totalHits} images.`);
-    }
-    if (!hits.length) {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
-    }
-  } catch (error) {
-    // Notify.warning(
-    //   `We're sorry, but you've reached the end of search results.`
-    // );
-  }
-}
+const observer = new IntersectionObserver(updateGallery, observOptions);
 
 function updateGallery(entries) {
   entries.forEach(entry => {
@@ -85,4 +37,58 @@ function updateGallery(entries) {
       pixabayAPI(nameRequest, (page += 1));
     }
   });
+}
+
+function onFormSbmt(evt) {
+  evt.preventDefault();
+
+  if (!evt.currentTarget.elements.input.value) {
+    Notify.warning('Please, enter a search term!');
+    evt.preventDefault();
+    return false;
+  }
+  if (nameRequest === evt.currentTarget.elements.input.value) {
+    Notify.warning(`It's already found! May be another one?`);
+    return;
+  }
+  nameRequest = evt.currentTarget.elements.input.value;
+
+  evt.target.reset();
+  form.classList.remove('open');
+  galleryRef.innerHTML = '';
+  page = 1;
+
+  pixabayAPI(nameRequest, page);
+}
+
+async function pixabayAPI(nameRequest, page) {
+  observer.unobserve(guard); //если новый поиск вернет не более одной страницы
+
+  try {
+    const data = await fetchPics(nameRequest, page);
+    const hits = await data.hits;
+
+    if (!hits.length) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    renderMarkup(hits, galleryRef, gap);
+    gallery.refresh();
+
+    if (hits.length === 40) {
+      observer.observe(guard);
+    }
+
+    if (page === 1 && hits.length > 1) {
+      Notify.info(`Hooray! We found ${data.totalHits} images.`);
+    }
+  } catch (error) {
+    Notify.warning(
+      `We're sorry, but you've reached the end of search results.`
+    );
+    observer.unobserve(guard);
+  }
 }
