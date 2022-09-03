ca15a632f211573212f476f4f225f8add54bf685
Denis Orlyansky <000slim001@gmail.com>
2022-09-03 16:15:59 +0300

    fix fix fix

--- src/js/fetch-img.js
+++ src/js/fetch-img.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 const API_KEY = '29482011-99768188be0395583a9f1e73d';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchPics(query, page) {
+export async function fetchPics(q, page) {
   const params = {
     key: API_KEY,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
     per_page: 40,
-    query,
     page,
+    q,
   };
 
   const response = await axios.get(BASE_URL, { params });
